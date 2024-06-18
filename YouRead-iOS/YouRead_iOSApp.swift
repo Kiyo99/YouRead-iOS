c0aebb6883e793e343d06cd794da975e4eba8311
kiyo99 <kiofavour@gmail.com>
2024-06-18 09:44:46 -0400

    Initial Commit

---
+++ YouRead-iOS/YouRead_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouRead_iOSApp.swift
+//  YouRead-iOS
+//
+//  Created by Godsfavour Ngo Kio on 2024-06-18.
+//
+
+import SwiftUI
+
+@main
+struct YouRead_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
